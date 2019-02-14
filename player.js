25d229f41b790e1bfe0013bd8934f1ded0af7246
dezrenmikell <dezren.mikell@yahoo.com>
2019-02-14 16:57:08 -0500

    added sound to divs

--- player.js
+++ player.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    
+    $('.pikachu').on('click', function(){
+        $('.pika').trigger('play');
+    })
+    $('.charmander').on('click', function(){
+        $('.charr').trigger('play');
+    })
+    $('.squirtle').on('click', function(){
+        $('.squirt').trigger('play');
+    })
+    $('.bulbs').on('click', function(){
+        $('.bulba').trigger('play');
+    })
 });
