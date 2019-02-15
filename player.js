5c54cb899326ecb1e0706ba6bb581401a38ed635
dezrenmikell <dezren.mikell@yahoo.com>
2019-02-15 10:37:15 -0500

    connected randomizer with switch statement

--- player.js
+++ player.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
+    //generate random numbers (between 1&4)to select between different divs
+    const pokeChoice = [$('.pikachu'), $('.charmander'), $('.squirtle'), $('.bulbs')];
+    
+    var selector = Math.floor((Math.random()*4)+1);
+    console.log(selector);
+
+    //create switch statements to pick sound and animation for the random location chosen
+    switch(selector){
+        //pikachu
+        case 1:
+            console.log('pika');
+         break;
+        //charmander
+         case 2:
+            console.log('char');
+          break;
+        //squirtle 
+          case 3:
+            console.log('squirt');
+           break;
+        //bulbasaur
+          case 4:
+            console.log('bulba');
+            break;
+
+    }
+    //create event listeners for player moves
     $('.pikachu').on('click', function(){
         $('.pika').trigger('play');
     })
