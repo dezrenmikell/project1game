a4034a380e2e16b37b657794da54431a82b9e0b1
dezrenmikell <dezren.mikell@yahoo.com>
2019-02-15 15:23:57 -0500

    added functionality to the random number, but sound is only active on click

--- player.js
+++ player.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+
+    //make number randomizer
+   var selector= Math.floor((Math.random() * 4) + 1);
+   //make switch statement to access divs
+   switch(selector){
+       case 1:
+       document.querySelector('.pika').play();
+       $( ".pikachu" ).toggle( "scale" );
+       $( ".pikachu" ).toggle( "scale" );
+       break;
+
+       case 2:
+       document.querySelector('.charr').play();
+       $( ".charmander" ).toggle( "scale" );
+       $( ".charmander" ).toggle( "scale" );
+       break;
+
+       case 3:
+       document.querySelector('.squirt').play();
+       $( ".squirtle" ).toggle( "scale" );
+       $( ".squirtle" ).toggle( "scale" );
+       break;
+
+       case 4:
+       document.querySelector('.bulba').play();
+       $( ".bulbs" ).toggle( "scale" );
+       $( ".bulbs" ).toggle( "scale" );
+       break;
+   }
+
     $( ".pikachu" ).click(function() {
         document.querySelector('.pika').play();
         $( ".pikachu" ).toggle( "scale" );
