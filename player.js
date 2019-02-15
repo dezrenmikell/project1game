ad4992d6f71f1484d0f163fc699c36bd4f3d8d0a
dezrenmikell <dezren.mikell@yahoo.com>
2019-02-15 14:36:01 -0500

    created image span and sound toggle

--- player.js
+++ player.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
     $( ".pikachu" ).click(function() {
+        document.querySelector('.pika').play();
         $( ".pikachu" ).toggle( "scale" );
         $( ".pikachu" ).toggle( "scale" );
-      });
+        
+    });
       $( ".bulbs" ).click(function() {
+        document.querySelector('.bulba').play();
         $( ".bulbs" ).toggle( "scale" );
         $( ".bulbs" ).toggle( "scale" );
       });
       $( ".charmander" ).click(function() {
+        document.querySelector('.charr').play();
         $( ".charmander" ).toggle( "scale" );
         $( ".charmander" ).toggle( "scale" );
       });
       $( '.squirtle' ).click(function() {
+        document.querySelector('.squirt').play();
         $( ".squirtle" ).toggle( "scale" );
         $( ".squirtle" ).toggle( "scale" );
       });
