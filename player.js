f133020f4fec9e24902a12533cd02e695ab1511e
dezrenmikell <dezren.mikell@yahoo.com>
2019-02-18 20:56:21 -0500

    added pkemon functions into number randomizer and deleted excess code, for the moment

--- player.js
+++ player.js
@@ -4,44 +4,31 @@ $(document).ready(function(){
     var humPlayer = [];
     var pusher=0;
     
-    //start game logic  
-    var controller1=true; 
-    var controller2=true;
-    //while loop to keep game playing    
-       //while(controller1==true){
-         //  while (controller2==true){
-
     //make number randomizer
    var selector = Math.floor((Math.random() * 4) + 1);
    //make switch statement to access divs
    if(selector==1){
        
-       $( ".pikachu" ).toggle( "scale" );
-       $( ".pikachu" ).toggle( "scale" );
+      pikaChoose();
         comPlayer.push(selector);
     }
        else if(selector == 2){
-       
-       $( ".charmander" ).toggle( "scale" );
-       $( ".charmander" ).toggle( "scale" );
+       charChoose();
        comPlayer.push(selector);
         }
 
        else if( selector == 3){
-       
-       $( ".squirtle" ).toggle( "scale" );
-       $( ".squirtle" ).toggle( "scale" );
+       squirtChoose();
        comPlayer.push(selector);
         }
 
        else if( selector== 4){
        
-       $( ".bulbs" ).toggle( "scale" );
-       $( ".bulbs" ).toggle( "scale" );
+        bulbChoose();
        comPlayer.push(selector);
         }
-        controller2=false;
- if(comPlayer.length == (humPlayer.length +1)){
+       
+ 
 
 //create player actions
     $( ".pikachu" ).click(function() {
@@ -80,12 +67,10 @@ $(document).ready(function(){
       console.log(comPlayer);
       console.log('yo');
       console.log(humPlayer);
-    }
-    else{
+    
+    
        controller1=false;
        console.log('game over');
-    }
-    //}
-//}
+   
    console.log(humPlayer);
 });
