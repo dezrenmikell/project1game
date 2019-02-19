194d8f4a1a43b3e7c2f66eeddf73e0a49be3bc9a
dezrenmikell <dezren.mikell@yahoo.com>
2019-02-18 21:11:42 -0500

    replaced if statements with switch statement for better readability

--- player.js
+++ player.js
@@ -4,73 +4,58 @@ $(document).ready(function(){
     var humPlayer = [];
     var pusher=0;
     
-    //make number randomizer
+  //make number randomizer
    var selector = Math.floor((Math.random() * 4) + 1);
    //make switch statement to access divs
-   if(selector==1){
-       
-      pikaChoose();
-        comPlayer.push(selector);
-    }
-       else if(selector == 2){
-       charChoose();
-       comPlayer.push(selector);
-        }
+   switch(selector){
+     case 1:
+     pikaChoose();
+     comPlayer.push(selector);
+     break;
 
-       else if( selector == 3){
-       squirtChoose();
-       comPlayer.push(selector);
-        }
+     case 2:
+     charChoose();
+     comPlayer.push(selector);
+     break;
 
-       else if( selector== 4){
-       
-        bulbChoose();
-       comPlayer.push(selector);
-        }
-       
- 
+     case 3:
+     squirtChoose();
+     comPlayer.push(selector);
+     break;
 
+     case 4:
+     bulbChoose();
+     comPlayer.push(selector);
+     break;
+   }
+   
 //create player actions
-    $( ".pikachu" ).click(function() {
+    $('.pikachu').click(function() {
         pikaChoose();
         pusher =1;
         console.log(pusher);
         humPlayer.push(pusher);
-        controller2=true;
-
-        
+        controller2=true;       
     });
       
-      $( ".charmander" ).click(function() {
+      $('.charmander').click(function() {
         charChoose();
         pusher=2;
         humPlayer.push(pusher);
         console.log(pusher);
-        controller2=true;
       });
-      $( '.squirtle' ).click(function() {
+      
+      $('.squirtle').click(function() {
         squirtChoose();
         pusher=3;
         humPlayer.push(pusher);
         console.log(pusher);
-        controller2=true;
-        });
-      $( ".bulbs" ).click(function() {
+      });
+      
+      $('.bulbs').click(function() {
         bulbChoose();
         pusher=4;
         humPlayer.push(pusher);
         console.log(pusher);
-        controller2=true;
-    });
-    
-
-      console.log(comPlayer);
-      console.log('yo');
-      console.log(humPlayer);
-    
-    
-       controller1=false;
-       console.log('game over');
-   
-   console.log(humPlayer);
+      });
 });
