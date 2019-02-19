155eedf21aa9f624fceb075f5f8e06c7c5bc0db5
dezrenmikell <dezren.mikell@yahoo.com>
2019-02-18 21:17:54 -0500

    cleaned up code, getting ready to add promises

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
+function pikaChoose() {
+  document.querySelector(".pika").play();
+  $(".pikachu").toggle("scale");
+  $(".pikachu").toggle("scale");
+}
 
-    
-    function pikaChoose(){
-        document.querySelector('.pika').play();
-        $( ".pikachu" ).toggle( "scale" );
-        $( ".pikachu" ).toggle( "scale" );
-    }
+function charChoose() {
+  document.querySelector(".charr").play();
+  $(".charmander").toggle("scale");
+  $(".charmander").toggle("scale");
+}
 
-    function charChoose(){
-        document.querySelector('.charr').play();
-        $( ".charmander" ).toggle( "scale" );
-        $( ".charmander" ).toggle( "scale" );
-    }
+function squirtChoose() {
+  document.querySelector(".squirt").play();
+  $(".squirtle").toggle("scale");
+  $(".squirtle").toggle("scale");
+}
 
-    function squirtChoose(){
-        document.querySelector('.squirt').play();
-        $( ".squirtle" ).toggle( "scale" );
-        $( ".squirtle" ).toggle( "scale" );
-    }
-
-    function bulbChoose(){
-        document.querySelector('.bulba').play();
-        $( ".bulbs" ).toggle( "scale" );
-        $( ".bulbs" ).toggle( "scale" );
-    }
-
-
+function bulbChoose() {
+  document.querySelector(".bulba").play();
+  $(".bulbs").toggle("scale");
+  $(".bulbs").toggle("scale");
+}
--- player.js
+++ player.js
@@ -1,61 +1,60 @@
-$(document).ready(function(){
-//initialize player arrays
-    var comPlayer = [];
-    var humPlayer = [];
-    var pusher=0;
-    
+$(document).ready(function() {
+  //initialize player arrays
+  var comPlayer = [];
+  var humPlayer = [];
+  var pusher = 0;
+
   //make number randomizer
-   var selector = Math.floor((Math.random() * 4) + 1);
-   //make switch statement to access divs
-   switch(selector){
-     case 1:
-     pikaChoose();
-     comPlayer.push(selector);
-     break;
+  var selector = Math.floor(Math.random() * 4 + 1);
+  //make switch statement to access divs
+  switch (selector) {
+    case 1:
+      pikaChoose();
+      comPlayer.push(selector);
+      break;
 
-     case 2:
-     charChoose();
-     comPlayer.push(selector);
-     break;
+    case 2:
+      charChoose();
+      comPlayer.push(selector);
+      break;
 
-     case 3:
-     squirtChoose();
-     comPlayer.push(selector);
-     break;
+    case 3:
+      squirtChoose();
+      comPlayer.push(selector);
+      break;
 
-     case 4:
-     bulbChoose();
-     comPlayer.push(selector);
-     break;
-   }
-   
-//create player actions
-    $('.pikachu').click(function() {
-        pikaChoose();
-        pusher =1;
-        console.log(pusher);
-        humPlayer.push(pusher);
-        controller2=true;       
-    });
-      
-      $('.charmander').click(function() {
-        charChoose();
-        pusher=2;
-        humPlayer.push(pusher);
-        console.log(pusher);
-      });
-      
-      $('.squirtle').click(function() {
-        squirtChoose();
-        pusher=3;
-        humPlayer.push(pusher);
-        console.log(pusher);
-      });
-      
-      $('.bulbs').click(function() {
-        bulbChoose();
-        pusher=4;
-        humPlayer.push(pusher);
-        console.log(pusher);
-      });
+    case 4:
+      bulbChoose();
+      comPlayer.push(selector);
+      break;
+  }
+
+  //create player actions
+  $(".pikachu").click(function() {
+    pikaChoose();
+    pusher = 1;
+    console.log(pusher);
+    humPlayer.push(pusher);
+  });
+
+  $(".charmander").click(function() {
+    charChoose();
+    pusher = 2;
+    humPlayer.push(pusher);
+    console.log(pusher);
+  });
+
+  $(".squirtle").click(function() {
+    squirtChoose();
+    pusher = 3;
+    humPlayer.push(pusher);
+    console.log(pusher);
+  });
+
+  $(".bulbs").click(function() {
+    bulbChoose();
+    pusher = 4;
+    humPlayer.push(pusher);
+    console.log(pusher);
+  });
 });
